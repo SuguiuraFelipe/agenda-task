c74763804896c887c80f55cac7f3df3354c3ad7d
SuguiuraFelipe <felipesuguiura.pro@gmail.com>
2026-03-12 17:59:04 +0000

    Initial commit with project outline

---
+++ src/main/java/com/suguiura/Agendatask/AgendaTaskApplication.java
@@ -0,0 +1,13 @@
+package com.suguiura.Agendatask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suguiura/Agendatask/AgendaTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suguiura.Agendatask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
